cd9f6eca99d445d63539a4ae0f7a01beeccf8d35
RDHarHar <ryan.harwick@gmail.com>
2022-01-03 13:13:30 -0500

    More External Function Testing

--- Funct_External.py
+++ Funct_External.py
@@ -1,9 +1,49 @@
 # Run function from external script
 
-from Function_Testing import spell_name
+# from Function_Testing import spell_name
 
-user_name = input("Please enter your first name: ")
+# user_name = input("Please enter your first name: ")
 
-name_length = spell_name(user_name)
+# name_length = spell_name(user_name)
 
-print(f"You have {name_length} characters in your name.")
+# print(f"You have {name_length} characters in your name.")
+
+from func_Calc import *
+
+select_loop = 0
+valid = 0
+
+while select_loop == 0:
+    print("Please make a selection.\n\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n")
+    selection = input("Selection: ")
+
+    if selection == "1" or selection == "Addition" or selection == "addition":
+        str_selection = "Addition"
+        select_loop = 1
+    elif selection == "2" or selection == "Subtraction" or selection == "subtraction":
+        str_selection = "Subtraction"
+        select_loop = 1
+    elif selection == "3" or selection == "Multiplication" or selection == "multiplication":
+        str_selection = "Multiplcation"
+        select_loop = 1
+    elif selection == "4" or selection == "Division" or selection == "division":
+        str_selection = "Divison"
+        select_loop = 1
+    else:
+        print("**Error**\nInvalid input. Please input either a number (1-4) or the word for the selection you wish to make\n")
+        select_loop = 0
+
+    print(f"\nYou have selected {str_selection}. Please enter in the two numbers you would like to calculate.\n")
+    num1 = input("number 1: ")
+    num2 = input("number 2: ")
+
+    if str_selection == "Addition":
+        ans = add_num(num1, num2)
+    elif str_selection == "Subtraction":
+        ans = sub_num(num1, num2)
+    elif str_selection == "Multiplcation":
+        ans = mul_num(num1, num2)
+    elif str_selection == "Divison":
+        ans = div_num(num1, num2)
+
+    print(f"The answer is {ans}.")
---
+++ func_Calc.py
@@ -0,0 +1,17 @@
+# Function Calculator
+
+def add_num(num1, num2):
+    sum = int(num1) + int(num2)
+    return sum
+
+def sub_num(num1, num2):
+    sum = int(num1) - int(num2)
+    return sum
+
+def div_num(num1, num2):
+    sum = int(num1) / int(num2)
+    return sum
+
+def mul_num(num1, num2):
+    sum = int(num1) * int(num2)
+    return sum
